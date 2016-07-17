1c9afeeca6c0783d3f9b81f7e88fca8d6d2936ce
Doug Black Jr <dougblackjr@gmail.com>
2016-07-17 19:20:16 -0400

    Fixed comment redirect

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -28,13 +28,14 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to todo_path(@comment.todo_id), notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
+
   end
 
   # PATCH/PUT /comments/1
@@ -56,7 +57,7 @@ class CommentsController < ApplicationController
   def destroy
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
+      format.html { redirect_to todo_path(@comment.todo_id), notice: 'Comment was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
